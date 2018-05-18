8c473f4f5df00fd26621f250676140026d526fb5
Leonardo <leolucasm@hotmail.com>
2018-05-17 22:21:13 -0300

    Inicializando Projeto

---
+++ src/rn/RN.java
@@ -0,0 +1,16 @@
+package rn;
+
+/**
+ *
+ * @author Leonardo
+ */
+public class RN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
